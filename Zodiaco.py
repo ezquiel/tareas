eade176485779f04188480be584f6d6a587fb220
ezquiel <32112815+ezquiel@users.noreply.github.com>
2017-09-19 18:12:28 -0400

    add zodiaco.py

---
+++ Zodiaco.py
@@ -0,0 +1,15 @@
+dia=input("dia")
+mes=input("mes")
+dia=int(dia)
+mes=int(mes)
+if dia> =1 and dia <=22
+print("el dia esta correcto")
+if mes > = 1 and mes <=20
+print("el mes esta correcto/n/n")
+if(mes == 1 and dia >=21)or(mes == 1 and dia <=20)
+print("el signo es capricornio")
+if dia> = 20 and dia <=20
+print(" el dia esta correcto")
+if mes >= 2 and mes <=2
+print("el mes esta correcto/n/n")
+if (mes == 2 and dia >=20) or (mes == 1 and dia <=20)
